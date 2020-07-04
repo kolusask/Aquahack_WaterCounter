13b91eb3eee57ccf733cd6f6d72b1366a79cf11c
kolusask <19ak97@gmail.com>
2020-07-04 22:45:32 +0200

    Enabled photo capturing and sending to database

--- Image_recognition.py
+++ Image_recognition.py
@@ -1,16 +1,14 @@
 import requests
 import string
+import os
+from datetime import date,datetime
 
-
- #First version of code, will be updated!
-
-
+# Connecting to OCR server
 def ocr_space_file(filename, api_key='07cf5a4f8488957'):
-
+    #Parameters of OCR
     payload = {
                'apikey': api_key,
                'OCREngine':'2',
-                #'scale': True
                }
     with open(filename, 'rb') as f:
         r = requests.post('https://api.ocr.space/parse/image',
@@ -18,46 +16,79 @@ def ocr_space_file(filename, api_key='07cf5a4f8488957'):
                           data=payload,
                           )
 
+    #Formatting the obtained data
     data1=r.json()
     al=(data1["ParsedResults"][0]["ParsedText"])
-    leg=[]
-    zag=[]
-    leg.append(al)
+    letter_remove=[]
     a=al.split()
-    for i in range (len(a)-1):
-        #print('ici',a[i][0])
-        if a[i][0] not in string.ascii_letters :
-             zag.append(a[i])
-    gag =[]
+    formatted_list = []
 
-
+    #Using simple filter for wrong recognition of numbers
     for i in a:
         mesi=i.replace("O","0")
-        gag.append(mesi)
+        fin_l=mesi.replace("o","0")
+        formatted_list.append(fin_l)
+
+    # adding only digits to the list
+    for i in range (len(formatted_list)-1):
+        if (formatted_list[i][0]=='0') :
+            letter_remove.append(formatted_list[i])
 
 
+    second_filter = []
 
-    answer_list = []
-    for i in gag:
+    #double check for the validity of all inputs of numbers
+    for i in letter_remove:
         if (i.isnumeric() == True):
-            answer_list.append(i)
+            second_filter.append(i)
 
     fin_list=[]
-    for i in answer_list:
+    # Seperating numbers just in case they are recognized as together
+    # (such as 7 and 5 on counter may be recognized as 75)
+    for i in second_filter:
         if (len(i)>1):
             for j in i:
                 fin_list.append(j)
         else:
             fin_list.append(i)
 
+
+    #Getting current time and date for sending to the server
+    today=date.today()
+    timing = datetime.now().time()
+    # convert all strings into the integer
     fin_list = [int(i) for i in fin_list]
 
+    #Combining seperate numbers to one integer
+    stri=[str(integer) for integer in fin_list]
+    res = int("".join(stri))
 
-    print('ANswer = ', fin_list)
-    print(data1["ParsedResults"][0]["ParsedText"])
+    #creating dictionary for sending it to the database
+    send_dict =  {
+       'date':today,
+        'time':timing,
+        'reading':res
+                }
+    #Creating request for posting data
+    requests.post('https://aquahackwatercounter.azurewebsites.net/api/putNewReading',params=send_dict,headers={"x-functions-key":"CZoQdv4HoXStabracJbjZFH5HmuocietVuyw5vBJTXXpZJDTgbW4VA=="})
 
-# Use examples:
 
 
-ocr_space_file(filename='15.jpeg')
+    #For Printing final result
+    print('Counter Result = ', res)
+
+
+
+#######     Function for Raspberry Pi to take a photo
+
+##### for taking photo instantly with Raspberry Pi we uncomment line 87-88 and change filename
+##### accordingly while calling function
+
+#while True:
+    #os.system('fswebcam -r 640x480 -S 3 --jpeg 50 --save /home/pi/Samples/counter_meter.jpg')
+
+##### for checking photo only from local storage
+
+ocr_space_file(filename='test_photo.jpg')
+
 
